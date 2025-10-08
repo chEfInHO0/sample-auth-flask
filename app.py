a19f8d53f2f7566cc991b96700fdb23f54cc5eb9
Luccas Santos <luccaselias0@gmail.com>
2025-10-08 13:26:47 -0300

    refactor the app.py removing and moving the db logic to the models for best practices

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
+from models import db, User
 
 
-@app.route("/", methods=["GET"])
-def home():
-    return "Home"
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = "your_secret_key"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+    db.init_app(app)
+
+    @app.route("/", methods=["GET"])
+    def home():
+        user_count = db.session.execute(db.select(User)).scalars().all()
+        return f"Home. Total de usuários no DB: {len(user_count)}"
+
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
