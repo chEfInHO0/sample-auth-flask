7f29ef9df713fa8e445bbcebd3c57365235b86d7
Luccas Santos <luccaselias0@gmail.com>
2025-10-10 16:28:07 -0300

    adding functions to Read information on database

--- utils/db_helper.py
+++ utils/db_helper.py
@@ -1,12 +1,8 @@
 from sqlalchemy.orm import Session
-from flask import jsonify
+from sqlalchemy import select
 from models import db
 
 def commit_session(obj):
-    """
-    Adiciona um objeto à sessão e faz o commit de forma segura.
-    Retorna True em caso de sucesso, ou uma mensagem de erro.
-    """
     try:
         with Session(db.engine) as session:
             session.add(obj)
@@ -14,3 +10,32 @@ def commit_session(obj):
         return True, None
     except Exception as e:
         return False, str(e)
+
+def fetch_all(model, filters=None):
+    """
+    Executa um SELECT * FROM model com filtros opcionais.
+    Retorna uma lista de objetos.
+    """
+    try:
+        with Session(db.engine) as session:
+            stmt = select(model)
+            if filters:
+                stmt = stmt.filter_by(**filters)
+            result = session.execute(stmt)
+            return result.scalars().all(),None
+    except Exception as e:
+        return None, str(e)
+
+def fetch_one(model, filters=None):
+    """
+    Executa um SELECT com filtros e retorna apenas um resultado.
+    """
+    try:
+        with Session(db.engine) as session:
+            stmt = select(model)
+            if filters:
+                stmt = stmt.filter_by(**filters)
+            result = session.execute(stmt)
+            return result.scalars().first(),None
+    except Exception as e:
+        return None, str(e)
