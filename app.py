188a1c82b690cc153a3309ea0ade83c0c0b8c9d7
Luccas Santos <luccaselias0@gmail.com>
2025-11-10 15:15:54 -0300

    refactor(app.py): refactor main file to apply project organization changes
    added new endpoints for database tests

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 
-from flask import Flask
-from models import db, User
+from flask import Flask, request, jsonify
+from database import db
+from models.users import Users
+from schemas.users import UserCreate
+from pydantic import ValidationError
+from sqlalchemy.exc import IntegrityError
+from middleware.sqlErrorHandler import SqlErrorHandler
 
 
 def create_app():
@@ -11,18 +16,32 @@ def create_app():
 
     db.init_app(app)
 
-    @app.route("/", methods=["GET"])
-    def home():
-        user_count = db.session.execute(db.select(User)).scalars().all()
-        return f"Home. Total de usuários no DB: {len(user_count)}"
+    @app.route("/user", methods=["GET"])
+    def get_user():
+        users = Users.query.all()
+        return jsonify({"users":[u.to_dict() for u in users]})
 
+    @app.route('/user', methods=['POST'])
+    def post_user():
+        if not request.get_json():
+            return jsonify({"message": "Content missing"}), 400
+        try:
+            data = UserCreate(**request.get_json())
+        except ValidationError as e:
+            return jsonify({"message": e.errors()}), 400
+        try:
+            user = Users(**data.model_dump())
+
+            db.session.add(user)
+            db.session.commit()
+        except IntegrityError as e:
+            error_details = SqlErrorHandler(e).errors()
+            return jsonify(error_details),error_details.get("status_code")
+        return jsonify({"message": user.to_dict()}), 201
     return app
 
 
 if __name__ == '__main__':
     app = create_app()
 
-    with app.app_context():
-        db.create_all()
-
-    app.run(debug=True)
+    app.run(debug=True, port=3333)
