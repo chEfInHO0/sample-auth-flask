e8a2f737ad1278b558b6b995c795b320f93d5203
Luccas Santos <luccaselias0@gmail.com>
2025-11-10 15:02:29 -0300

    chore(database): create SQLAlchemy model and Pydantic schema for validation

---
+++ models/users.py
@@ -0,0 +1,18 @@
+from database import db
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    
+    def __repr__(self):
+        return f"{self.user_id} - {self.username} - {self.email}"
+
+    def to_dict(self):
+        return {
+            "user_id":self.user_id,
+            "username":self.username,
+            "email":self.email
+        }
