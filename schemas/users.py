4078efee3f88f300bf578674343db4dd04a30c9f
Luccas Santos <luccaselias0@gmail.com>
2025-11-10 19:40:00 -0300

    chore: update model and schema to support login and update

--- schemas/users.py
+++ schemas/users.py
@@ -5,6 +5,14 @@ class UserCreate(BaseModel):
     email:str
     password:str
 
+class UserUpdate(BaseModel):
+    username:str
+    email:str
+    password:str
+
+class UserUpdatePassword(BaseModel):
+    password:str
+
 class UserLogin(BaseModel):
     username:str
     password:str
